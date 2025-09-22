6ce25f74caf3b78b88e7b97d253d821b32d55e35
Andy Ionita <andy.ionita@dawsoncollege.qc.ca>
2025-09-22 10:51:32 -0400

    Unit test if color values get clamped correctly

---
+++ ShapeLibraryTests/ColourTests.cs
@@ -0,0 +1,19 @@
+﻿using ShapeLibrary;
+
+namespace ShapeLibraryTests;
+
+[TestClass]
+public sealed class ColourTests
+{
+    [TestMethod]
+    public void Constructor_ClampsValuesCorrectly()
+    {
+        int red = -10, green = 260, blue = 128;
+
+        Colour colour = new Colour(red, green, blue);
+
+        Assert.AreEqual(0, colour.Red);
+        Assert.AreEqual(255, colour.Green);
+        Assert.AreEqual(128, colour.Blue);
+    }
+}
