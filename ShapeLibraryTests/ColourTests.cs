8e9af15de6c05eb3957dd864755982561c3911a3
Andy Ionita <andy.ionita@dawsoncollege.qc.ca>
2025-09-22 10:52:38 -0400

    Test the addition operator

--- ShapeLibraryTests/ColourTests.cs
+++ ShapeLibraryTests/ColourTests.cs
@@ -16,4 +16,17 @@ public sealed class ColourTests
         Assert.AreEqual(255, colour.Green);
         Assert.AreEqual(128, colour.Blue);
     }
+
+    [TestMethod]
+    public void TestAdditionOperator()
+    {
+        var colour1 = new Colour(200, 100, 50);
+        var colour2 = new Colour(100, 200, 255);
+
+        var result = colour1 + colour2;
+
+        Assert.AreEqual(255, result.Red);
+        Assert.AreEqual(255, result.Green);
+        Assert.AreEqual(255, result.Blue);
+    }
 }
