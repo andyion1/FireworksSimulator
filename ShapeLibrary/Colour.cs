5ea7ac993e497fc5440bf43dc423fc6f99a5b1b6
Andy Ionita <andy.ionita@dawsoncollege.qc.ca>
2025-09-22 10:23:43 -0400

    Start Colour struct and clamp function

---
+++ ShapeLibrary/Colour.cs
@@ -0,0 +1,28 @@
+﻿namespace ShapeLibrary;
+
+public struct Colour
+{
+    public int Red { get; }
+    public int Green { get; }
+    public int Blue { get; }
+    
+    public Colour()
+    {
+
+    }
+
+    public int ClampColorValue(int colorValue)
+    {
+        if (colorValue < 0)
+        {
+            return 0;
+        }
+        else if (colorValue > 255)
+        {
+            return 255;
+        }
+
+        return colorValue;
+
+    }
+}
