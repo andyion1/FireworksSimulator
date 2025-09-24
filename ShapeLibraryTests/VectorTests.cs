d17da318e4a9b5b84d27f1c3912a27252bb8edcf
Andy Ionita <andy.ionita@dawsoncollege.qc.ca>
2025-09-24 09:50:23 -0400

    Test the vector addition/substraction

--- ShapeLibraryTests/VectorTests.cs
+++ ShapeLibraryTests/VectorTests.cs
@@ -12,7 +12,7 @@ namespace ShapeLibraryTests
     public sealed class VectorTests
     {
         [TestMethod]
-        public void TestConstructorInitialization()
+        public void TestConstructor_Initialization()
         {
             Vector v = new Vector(3.5f, -2f);
 
@@ -21,7 +21,7 @@ namespace ShapeLibraryTests
         }
 
         [TestMethod]
-        public void TestVectorReferencingValues()
+        public void TestVector_ReferencingValues()
         {
             Vector original = new Vector(1, 2);
 
@@ -40,5 +40,30 @@ namespace ShapeLibraryTests
 
             Assert.AreEqual("(5, 6)", result);
         }
+
+        [TestMethod]
+        public void Addition_Works()
+        {
+            Vector vector1 = new Vector(1, 2);
+            Vector vector2 = new Vector(3, 4);
+
+            Vector result = vector1 + vector2;
+
+            Assert.AreEqual(4, result.X);
+            Assert.AreEqual(6, result.Y);
+        }
+
+        [TestMethod]
+        public void Subtraction_Works()
+        {
+            var vector1 = new Vector(5, 6);
+            var vector2 = new Vector(2, 3);
+
+            var result = vector1 - vector2;
+
+            Assert.AreEqual(3, result.X);
+            Assert.AreEqual(3, result.Y);
+        }
+
     }
 }
