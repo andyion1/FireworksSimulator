40b5cefb61cf3bcdff4839d58ceaeab195f9da05
Andy Ionita <andy.ionita@dawsoncollege.qc.ca>
2025-09-24 09:42:41 -0400

    Add Vector constructor value testing

---
+++ ShapeLibraryTests/VectorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ShapeLibrary;
+
+namespace ShapeLibraryTests
+{
+    [TestClass]
+    public sealed class VectorTests
+    {
+        [TestMethod]
+        public void TestConstructorInitialization()
+        {
+            var v = new Vector(3.5f, -2f);
+            Assert.AreEqual(3.5f, v.X);
+            Assert.AreEqual(-2f, v.Y);
+        }
+    }
+}
