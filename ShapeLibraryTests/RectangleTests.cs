4c086ea62cf67957c18f503d858554a27223a7cd
Andy Ionita <andy.ionita@dawsoncollege.qc.ca>
2025-10-01 10:51:26 -0400

    Refactor unit tests for readability

--- ShapeLibraryTests/RectangleTests.cs
+++ ShapeLibraryTests/RectangleTests.cs
@@ -1,4 +1,5 @@
-﻿using ShapeLibrary;
+﻿using ShapeLib;
+using ShapeLibrary;
 using System.Drawing;
 
 namespace ShapeLibraryTests;
@@ -6,25 +7,35 @@ namespace ShapeLibraryTests;
 [TestClass]
 public sealed class RectangleTests
 {
+    private float x = 50f;
+    private float y = 50f;
+    private float width = 100f;
+    private float height = 100f;
+
+    private Colour DefaultColour = new Colour(100, 150, 200);
+
     [TestMethod]
     public void Constructor_AssignsPropertiesCorrectly()
     {
-        Colour colour = new Colour(100, 150, 200);
-        Rectangle rectangle = new Rectangle(50, 50, 100, 100, colour);
+        var rectangle = new Rectangle(x, y, width, height, DefaultColour);
 
-        Assert.AreEqual(50f, rectangle.X);
-        Assert.AreEqual(50f, rectangle.Y);
-        Assert.AreEqual(100f, rectangle.Width);
-        Assert.AreEqual(100f, rectangle.Height);
-        Assert.AreEqual(colour, rectangle.Colour);
+        Assert.AreEqual(x, rectangle.X);
+        Assert.AreEqual(y, rectangle.Y);
+        Assert.AreEqual(width, rectangle.Width);
+        Assert.AreEqual(height, rectangle.Height);
+        Assert.AreEqual(DefaultColour, rectangle.Colour);
     }
 
     [TestMethod]
     public void Vertices_CalculatedCorrectly()
     {
-        Colour colour = new Colour(100, 150, 200);
-        Rectangle rectangle = new ShapeLibrary.Rectangle(50, 50, 100, 100, colour);
+        var rectangle = new Rectangle(x, y, width, height, new Colour(255, 0, 0));
 
+        var vertices = rectangle.Vertices;
 
+        Assert.AreEqual($"({x}, {y})", vertices[0].ToString());
+        Assert.AreEqual($"({x + width}, {y})", vertices[1].ToString());
+        Assert.AreEqual($"({x + width}, {y + height})", vertices[2].ToString());
+        Assert.AreEqual($"({x}, {y + height})", vertices[3].ToString());
     }
 }
