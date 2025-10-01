bb8b9c229ebfe5a56d889c7791bcd664e221750b
Andy Ionita <andy.ionita@dawsoncollege.qc.ca>
2025-10-01 10:37:00 -0400

    Test if Rect constructor assigns properties correctly

--- ShapeLibraryTests/RectangleTests.cs
+++ ShapeLibraryTests/RectangleTests.cs
@@ -5,5 +5,16 @@ namespace ShapeLibraryTests;
 [TestClass]
 public sealed class RectangleTests
 {
-    
+    [TestMethod]
+    public void Constructor_AssignsPropertiesCorrectly()
+    {
+        Colour colour = new Colour(100, 150, 200);
+        Rectangle rectangle = new Rectangle(50, 50, 100, 100, colour);
+
+        Assert.AreEqual(50f, rectangle.X);
+        Assert.AreEqual(50f, rectangle.Y);
+        Assert.AreEqual(100f, rectangle.Width);
+        Assert.AreEqual(100f, rectangle.Height);
+        Assert.AreEqual(colour, rectangle.Colour);
+    }
 }
