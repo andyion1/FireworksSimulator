e49b86d5ce8af18df4dfed0fdcd7c1c678a140e2
Andy Ionita <andy.ionita@dawsoncollege.qc.ca>
2025-10-01 10:18:44 -0400

    Implement the start of IRectangle implementation

---
+++ ShapeLibrary/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿using ShapeLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeLibrary
+{
+    public class Rectangle : IRectangle
+    {
+        public float X { get; }
+        public float Y { get; }
+        public float Width { get; }
+        public float Height { get; }
+        public Colour Colour { get; }
+
+        private List<Vector>? _vertices;
+    }
+}
