51eb896d561f3d1e11d2b3900a5bc03c9ed71542
Andy Ionita <andy.ionita@dawsoncollege.qc.ca>
2025-10-06 09:18:06 -0400

    Create Circle class with interface properties

---
+++ ShapeLibrary/Circle.cs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeLibrary
+{
+    internal class Circle
+    {
+        public Vector Center { get; }
+        public float Radius { get; }
+    }
+}
