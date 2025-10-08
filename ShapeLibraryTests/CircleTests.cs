0e1fd59e818ec93b9648ee522a8ef3e1f90bad65
Andy Ionita <andy.ionita@dawsoncollege.qc.ca>
2025-10-08 09:21:48 -0400

    Test circle constructor has valid arguments

--- ShapeLibraryTests/CircleTests.cs
+++ ShapeLibraryTests/CircleTests.cs
@@ -5,9 +5,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ShapeLibraryTests
-{
+namespace ShapeLibraryTests;
+    
+    [TestClass]
     public sealed class CircleTests
     {
+        [TestMethod]
+        public void Constructor_ValidArguments_CreatesCircle()
+        {
+            float expectedX = 100f;
+            float expectedY = 200f;
+            float expectedRadius = 50f;
+            Colour expectedColour = new Colour(255, 0, 0);
+
+            Circle circle = new Circle(expectedX, expectedY, expectedRadius, expectedColour);
+
+            Assert.AreEqual(expectedX, circle.Center.X);
+            Assert.AreEqual(expectedY, circle.Center.Y);
+            Assert.AreEqual(expectedRadius, circle.Radius);
+            Assert.AreEqual(expectedColour, circle.Colour);
+        }
     }
-}
