639a631c9cb0cb16d44aa810138e36d6bd304e0e
Andy Ionita <andy.ionita@dawsoncollege.qc.ca>
2025-10-08 10:50:14 -0400

    Implement propreties for particle class

---
+++ ShapeLibrary/Particle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeLibrary;
+    internal class Particle
+    {
+        public Vector Acceleration { get; }
+        public Vector Velocity { get; }
+        public Vector Position { get; }
+        public ICircle Circle { get; }
+        public Colour Colour { get; }
+        public bool Done { get; }
+    }
