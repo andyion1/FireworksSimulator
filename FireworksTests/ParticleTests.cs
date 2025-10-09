da30eee41e29354ab1cc0e1886db65accefd1c5c
Andy Ionita <andyionita06@gmail.com>
2025-10-08 22:49:29 -0400

    Test that particle moves, lifespan decreases

--- FireworksTests/ParticleTests.cs
+++ FireworksTests/ParticleTests.cs
@@ -49,4 +49,18 @@ public sealed class ParticleTests
         Assert.AreEqual(1f, particle.Velocity.X, 0.001);
         Assert.AreEqual(2f, particle.Velocity.Y, 0.001);
     }
+
+    [TestMethod]
+    public void Update_ShouldMoveParticleAndDecreaseLifespan()
+    {
+        Colour colour = new Colour(255, 255, 0);
+        Particle particle = new Particle(0f, 0f, colour, 3);
+
+        particle.ApplyVelocity(new Vector(1f, 1f));
+        particle.Update();
+
+        Assert.AreEqual(1f, particle.Position.X, 0.001);
+        Assert.AreEqual(1f, particle.Position.Y, 0.001);
+        Assert.AreEqual(false, particle.Done);
+    }
 }
