d1ec3afec7a4c2e0c5aa8a681643a1eb18ce8add
Andy Ionita <andyionita06@gmail.com>
2025-10-08 22:58:58 -0400

    Fix lifespan expire test

--- Fireworks/Particle.cs
+++ Fireworks/Particle.cs
@@ -10,59 +10,60 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("FireworksTests")]
 
 namespace Fireworks;
-    internal class Particle : IParticle
+internal class Particle : IParticle
+{
+    private int _lifespan;
+    private bool _done;
+
+    public Vector Acceleration { get; private set; }
+    public Vector Velocity { get; private set;  }
+    public Vector Position { get; private set;  }
+    public ICircle Circle { get; private set;  }
+    public Colour Colour { get; private set;  }
+    public bool Done => _done;
+
+
+public Particle(float x, float y, Colour colour, int lifespan)
     {
-        private int _lifespan;
-        private bool _done;
+        Position = new Vector(x, y);
+        Colour = colour;
+        _lifespan = lifespan;
 
-        public Vector Acceleration { get; private set; }
-        public Vector Velocity { get; private set;  }
-        public Vector Position { get; private set;  }
-        public ICircle Circle { get; private set;  }
-        public Colour Colour { get; private set;  }
-        public bool Done { get; }
 
-        public Particle(float x, float y, Colour colour, int lifespan)
+        Circle = new Circle(x, y, 2f, colour);
+
+        Acceleration = new Vector(0, 0);
+        Velocity = new Vector(0, 0);
+    }
+
+    public void ApplyGravity()
+    {
+        Acceleration += new Vector(0, 0.1f);
+    }
+
+    public void ApplyVelocity(Vector velocity)
+    {
+        Velocity += velocity;
+    }
+
+    public void Update()
+    {
+        if (_done)
         {
-            Position = new Vector(x, y);
-            Colour = colour;
-            _lifespan = lifespan;
-
-
-            Circle = new Circle(x, y, 2f, colour);
-
-            Acceleration = new Vector(0, 0);
-            Velocity = new Vector(0, 0);
+            return;
         }
 
-        public void ApplyGravity()
+        Velocity += Acceleration;
+        Position += Velocity;
+
+        _lifespan--;
+        if (_lifespan <= 0)
         {
-            Acceleration += new Vector(0, 0.1f);
+            _done = true;
+            return;
         }
 
-        public void ApplyVelocity(Vector velocity)
-        {
-            Velocity += velocity;
-        }
-
-        public void Update()
-        {
-            if (_done)
-            {
-                return;
-            }
-
-            Velocity += Acceleration;
-            Position += Velocity;
-
-            _lifespan--;
-            if (_lifespan <= 0)
-            {
-                _done = true;
-                return;
-            }
-
-            Acceleration = new Vector(0, 0);
-            Circle = new Circle(Position.X, Position.Y, 2f, Colour);
-        }
+        Acceleration = new Vector(0, 0);
+        Circle = new Circle(Position.X, Position.Y, 2f, Colour);
+    }
 }
