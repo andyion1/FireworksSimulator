04386b992ef7847a700771a2d1b1b2a4437c4b74
Andy Ionita <andy.ionita@dawsoncollege.qc.ca>
2025-10-15 10:18:23 -0400

    Add pattern, weigth & width properties to fireworks

---
+++ Fireworks/Firework.cs
@@ -0,0 +1,22 @@
+﻿using ShapeLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fireworks
+{
+    internal class Firework : IFirework
+    {
+        private int _width;
+        private int _height;
+        private IExplosionPattern _pattern;
+
+
+        public Firework(int width, int height, Colour colour, IExplosionPattern pattern)
+        {
+
+        }
+    }
+}
