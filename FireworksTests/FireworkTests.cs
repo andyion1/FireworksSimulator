cd58d3892a6b2571d9c2183209ffb4f4372edcfb
Andy Ionita <andyionita06@gmail.com>
2025-10-20 15:16:43 -0400

    Make the FireworkTests file with the constants

---
+++ FireworksTests/FireworkTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShapeLibrary;
+
+namespace FireworksTests
+{
+    [TestClass]
+    public sealed class FireworkTests
+    {
+        private int DefaultWidth = 800;
+        private int DefaultHeight = 600;
+        private Colour DefaultColour = new Colour(255, 128, 0);
+        private int DefaultLifespan = 60;
+        private float DefaultLaunchSpeedY = -8f;
+        private float DefaultMinExplosionSpeed = 2.5f;
+        private float DefaultMaxExplosionSpeed = 6.5f;
+    }
+}
