708f501a3442a6c9dc5a056fa37230b5283ca45d
Andy Ionita <andyionita06@gmail.com>
2025-10-20 15:40:48 -0400

    Create FireworkEnvironmentTests with constants

---
+++ FireworksTests/FireworkEnvironmentTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fireworks;
+using ShapeLibrary;
+
+namespace FireworksTests
+{
+    [TestClass]
+    public sealed class FireworkEnvironmentTests
+    {
+        private int DefaultWidth = 800;
+        private int DefaultHeight = 600;
+        private Colour DefaultColour = new Colour(255, 128, 0);
+
+        private IExplosionPattern CreateDefaultPattern()
+        {
+            return new ExplosionPattern(-8f, 2.5f, 6.5f);
+        }
+    }
+}
