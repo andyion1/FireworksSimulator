d50128bd2279b9192c1e5fdcc427b4a309b77533
Andy Ionita <andyionita06@gmail.com>
2025-10-20 12:20:29 -0400

    Make constructor for ExplosionPattern.cs

--- Fireworks/ExplosionPattern.cs
+++ Fireworks/ExplosionPattern.cs
@@ -10,9 +10,27 @@ namespace Fireworks
 {
     internal class ExplosionPattern : IExplosionPattern
     {
-        private readonly Vector _launchVelocity = new Vector(0f, -8f);
+        private readonly Random _rng;
+        private readonly Vector _launchVelocity;
+        private readonly int _minParticles;
+        private readonly int _maxParticles;
+        private readonly float _minExplosionSpeed;
+        private readonly float _maxExplosionSpeed;
+
+        public ExplosionPattern(float launchSpeedY, int minParticles, int maxParticles, float minExplosionSpeed, float maxExplosionSpeed)
+        {
+            _rng = new Random();
+
+            _launchVelocity = new Vector(0f, launchSpeedY);
+            _minParticles = minParticles;
+            _maxParticles = maxParticles;
+            _minExplosionSpeed = minExplosionSpeed;
+            _maxExplosionSpeed = maxExplosionSpeed;
+        }
+
         public int NumberOfParticles => 60;
         public Vector ExplosionVelocity => new Vector(3f, 3f);
         public Vector LaunchVelocity => _launchVelocity;
     }
+
 }
