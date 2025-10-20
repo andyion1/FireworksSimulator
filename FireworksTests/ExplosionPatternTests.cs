b1f34d3a19dcf7c5c66af7cd5972604e8a09d99f
Andy Ionita <andyionita06@gmail.com>
2025-10-20 15:06:42 -0400

    Test if constructor has valid arguments for explosion pattern

--- FireworksTests/ExplosionPatternTests.cs
+++ FireworksTests/ExplosionPatternTests.cs
@@ -15,5 +15,18 @@ namespace FireworksTests
         private float DefaultLaunchSpeedY = -8f;
         private float DefaultMinExplosionSpeed = 2.5f;
         private float DefaultMaxExplosionSpeed = 6.5f;
+
+        [TestMethod]
+        public void Constructor_ValidArguments_CreatesExplosionPattern()
+        {
+            ExplosionPattern pattern = new ExplosionPattern(
+                DefaultLaunchSpeedY,
+                DefaultMinExplosionSpeed,
+                DefaultMaxExplosionSpeed
+            );
+
+            Assert.IsNotNull(pattern);
+            Assert.IsNotNull(pattern.LaunchVelocity);
+        }
     }
 }
