8f4904f44c4b1e703f1570953e1eb61fbbe341b3
Andy Ionita <andyionita06@gmail.com>
2025-10-20 14:32:31 -0400

    Make the screen scalable

--- FireworksSimulator/FireworksSimulator.cs
+++ FireworksSimulator/FireworksSimulator.cs
@@ -32,6 +32,11 @@ namespace FireworksSimulator
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+            Window.AllowUserResizing = true;
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.ApplyChanges();
         }
 
         protected override void Initialize()
