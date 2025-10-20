bc697f5794b244c15187c63be807223a6c89b329
Andy Ionita <andyionita06@gmail.com>
2025-10-20 13:23:45 -0400

    Create FireworkEnvironment class with list

---
+++ Fireworks/FireworkEnvironment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fireworks
+{
+    internal class FireworkEnvironment
+    {
+        private readonly List<IFirework> _fireworks;
+
+        public FireworkEnvironment()
+        {
+            _fireworks = new List<IFirework>();
+        }
+    }
+}
