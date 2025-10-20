c23a654948e572280376b7380222bf7a2f3c002b
Andy Ionita <andyionita06@gmail.com>
2025-10-20 12:13:38 -0400

    Add initial ExplosionPattern class w/ values

--- Fireworks/ExplosionPattern.cs
+++ Fireworks/ExplosionPattern.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using ShapeLibrary;
+
 namespace Fireworks
 {
-    internal class ExplosionPattern
+    internal class ExplosionPattern : IExplosionPattern
     {
+        private readonly Vector _launchVelocity = new Vector(0f, -8f);
+        public int NumberOfParticles => 60;
+        public Vector ExplosionVelocity => new Vector(3f, 3f);
+        public Vector LaunchVelocity => _launchVelocity;
     }
 }
